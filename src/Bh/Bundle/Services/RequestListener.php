9d8789c540fee42c9249165df0db8414948bc5bd
Tomasz Ratajczak <twratajczak@gmail.com>
2014-07-12 21:26:25 +0200

    403

--- src/Bh/Bundle/Services/RequestListener.php
+++ src/Bh/Bundle/Services/RequestListener.php
@@ -4,6 +4,7 @@ namespace Bh\Bundle\Services;
 
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class RequestListener
 {
@@ -16,6 +17,17 @@ class RequestListener
             $resp->headers->set('Access-Control-Allow-Method', 'GET, POST, DELETE');
             $resp->setStatusCode(Response::HTTP_OK);
             $resp->headers->set('X-Status-Code', 200);
+            $event->setResponse($resp);
+            return $event;
+        }
+
+        if ($event->getException() instanceOf AccessDeniedException) {
+            $resp = new Response();
+            $resp->headers->set('Access-Control-Allow-Origin', '*');
+            $resp->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Token');
+            $resp->headers->set('Access-Control-Allow-Method', 'GET, POST, DELETE');
+            $resp->setStatusCode(Response::HTTP_FORBIDDEN);
+            $resp->headers->set('X-Status-Code', 403);
             $event->setResponse($resp);
             return $event;
         }
